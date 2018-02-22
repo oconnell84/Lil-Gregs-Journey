05585695a5498ce004215a8b9f9d21fb7bd5c876
oconnell84 <36714935+oconnell84@users.noreply.github.com>
2018-02-22 15:20:31 -0500

    Healthbar
    added functional player Health bar, fixed damage numbers

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Slider healthBar;
+    public Text HPText;
+    public PlayerHealthManager playerHealth;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        healthBar.maxValue = playerHealth.playerMaxHP;
+        healthBar.value = playerHealth.playerCurrentHP;
+        HPText.text = "HP: " + playerHealth.playerCurrentHP + "/" + playerHealth.playerMaxHP;
+
+	}
+}
